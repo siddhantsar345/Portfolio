0c88f96ca251a7b8deaea3ffba44630ccb1fa3e3
siddhantsar345 <siddhantsar345@gmail.com>
2024-09-02 13:52:13 +0530

    Increased Responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,22 @@ const App=() => {
   return (
     <>
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-full w-full">
-        <div class="relative h-full w-full bg-black">
-          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:1.5vw_3vh]"></div>
-          <div class="absolute left-1/2 top-[-10%] transform -translate-x-1/2 w-[80vw] h-[80vw] max-w-[1000px] max-h-[1000px] rounded-full bg-[radial-gradient(circle_40%_at_50%_30%,#fbfbfb36,#000)]"></div>
-  </div>        
+  <div className="fixed top-0 -z-10 h-full w-full">
+    <div className="relative h-full w-full bg-black">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:1.5vw_3vh]"></div>
+      <div className="absolute left-1/2 top-[-10%] transform -translate-x-1/2 w-[80vw] h-[80vw] max-w-[90vw] max-h-[90vh] md:max-w-[800px] md:max-h-[800px] rounded-full bg-[radial-gradient(circle_40%_at_50%_30%,#fbfbfb36,#000)]"></div>
+    </div>
+  </div>
+  <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+    <Navbar />
+    <Hero />
+    <About />
+    <Experience />
+    <Projects />
+    <Contact />
+  </div>
 </div>
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <Navbar />
-          <Hero />
-          <About />
-          <Experience />
-          <Projects />
-          <Contact />
-        </div>
-      </div>
+
     </>
   )
 }
