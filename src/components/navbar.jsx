6a7d6e0f2e52399cce3d78dfa9d462b81502606e
siddhantsar345 <siddhantsar345@gmail.com>
2024-09-03 07:19:06 +0530

    Navbar responsiveness has been increased

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,21 +5,29 @@ import {FaSquareXTwitter} from "react-icons/fa6"
 import {FaInstagram} from "react-icons/fa"
 
 const Navbar= ()=>{
-    return <nav className="mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo" className="h-50 w-40 p-11 -m-0.5"/>
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl pr-10">
+    return (
+        <nav className="mb-20 flex flex-col md:flex-row items-center justify-between py-6">
+          {/* Logo Section */}
+          <div className="flex flex-shrink-0 items-center">
+            <img src={logo} alt="logo" className="h-12 w-auto p-3 md:h-16 md:w-auto md:p-0" />
+          </div>
+          
+          {/* Social Icons Section */}
+          <div className="mt-4 md:mt-0 flex items-center justify-center gap-4 text-xl md:text-2xl">
             <a href="https://www.linkedin.com/in/siddhant-sar/">
-            <FaLinkedin/></a>
+              <FaLinkedin />
+            </a>
             <a href="https://github.com/siddhantsar345">
-            <FaGithub/></a>
+              <FaGithub />
+            </a>
             <a href="https://www.instagram.com/siddhant__sar/">
-            <FaInstagram/></a>
+              <FaInstagram />
+            </a>
             <a href="https://x.com/sar_siddhant">
-            <FaSquareXTwitter/></a>
-        </div>
-    </nav>
-};
-
+              <FaSquareXTwitter />
+            </a>
+          </div>
+        </nav>
+      );
+    }
 export default Navbar;
