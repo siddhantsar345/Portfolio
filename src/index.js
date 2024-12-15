f4368f142c7ebf7ff5fdc37ebfe9e9eb89761a2c
siddhantsar345 <siddhantsar345@gmail.com>
2024-12-15 13:37:57 +0530

    Changes done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,14 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 export const EXPERIENCES = [
   {
     year: "July'23 - Present",
-    role: "Technical Assistant",
+    role: "Junior Technical Engineer",
     company: "WORKZ, Dubai",
-    description: `Responsible for supporting end to end testing of SIM(UICC), Responsible for supporting end to end testing of SIM(UICC), Review test plans with development team, Client interaction for troubleshooting purpose, Conducting weekly client interactions to provide project status updates and gather requirements for future projects.`,
-    technologies: ["Beyond Compare", "UL Spy", "SDMS, SFT, KMS"],
+    description: `Developed and Maintained JavaCard Applets for global SIM/eSIM vendors. Providing Technical Support, Bug fixes and ensuring timely delivery. 
+Responsible for supporting end to end testing of SIM(UICC).Support the investigation, troubleshooting and resolution of handset, card, network level issue 
+Review test plans with development team. Reporting of identified inefficiencies and defaults.Conducting weekly client interactions to provide project status updates and gather requirements for future projects. 
+Segregating customer data and creating data generation templates to support commercial production. Collaborated with cross-functional teams to streamline project workflows, contributing to a 10% increase in overall team productivity. 
+Requirement gathering and client interaction for troubleshooting purpose.BAP Tracker reporting, where we provide updates on all ongoing projects to the Senior Management Team and set deadlines to meet customer timelines. `,
+    technologies: ["Java Card, Native Card, Python,Beyond Compare", "UL Spy", "SDMS, SFT, KMS"],
   },
 ];
 
